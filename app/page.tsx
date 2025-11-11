ad3dcc8852578103445f429c23f2e644772f9e91
TEJAS <tejasdeshmane66@gmail.com>
2025-11-11 14:49:52 +0530

    Add authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import {Button} from "@/components/ui/button"
+import UserButton from "../features/auth/components/user-button";
 
 export default function Home() {
   return (
     <div>
       <Button>Click Me</Button>
+      <UserButton/>
     </div>
   );
 }
